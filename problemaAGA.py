40dd4f136a2cb574a7e293f7656e076f1d785610
gercardozo11-collab <gercardozo11@gmail.com>
2026-06-19 09:37:52 -0300

    Update problemaAGA.py

--- problemaAGA.py
+++ problemaAGA.py
@@ -8,7 +8,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("📡 Optimizador de Sensores")
+st.title("📡 Optimizador de instalacion de Sensores")
 
 # ==================================================
 # COBERTURA
